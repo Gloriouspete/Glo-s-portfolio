cedaab95e7e4f72ac12654a2355a685cc3446a0a
Gloriouspete <105151046+Gloriouspete@users.noreply.github.com>
2023-03-09 07:28:46 +0100

    Create replica.js

---
+++ replica.js
@@ -0,0 +1,20 @@
+let content,about,boutme;
+boutme = document.querySelector('.boutme');
+
+content = document.querySelector('.ncontent');
+
+about = document.querySelector('#nabout');
+
+about.addEventListener("click",() =>{
+    boutme.style.display="block"
+    content.style.display="none";
+    about.classList.add("agg");
+    about.textContent="X";
+    var mafor = document.querySelector('.agg');
+mafor.addEventListener("click", () =>{
+    boutme.style.display="none";
+    content.style.display="block";
+    about.textContent="About Me";
+    about.classList.remove("agg");
+})
+});
